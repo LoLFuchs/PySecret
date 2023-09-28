f07dc84cd7ba7416ff88db021ba00833ad33da44
Dominik Engelen <dominik.engelen@online.de>
2023-09-28 08:28:48 +0200

    gitignore added + some basic code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ root.resizable(False, False)
 root.geometry('500x500')
 root.eval("tk::PlaceWindow . center")
 
+
 mainFrame = tk.Frame(root)
 readFrame = tk.Frame(root)
 writeFrame = tk.Frame(root)
@@ -15,6 +16,21 @@ settingsFrame = tk.Frame(root)
 
 frame_List = [mainFrame,readFrame,writeFrame,settingsFrame]
 
+
+def gotoFrame(newFrame):
+    for frame in frame_List:
+        frame.pack_forget()
+    newFrame.pack()
+
+#--- Main Frame ---
+settingsButton = tk.Button(mainFrame,text="Settings",command=lambda: gotoFrame(settingsFrame))
+settingsButton.pack()
+
+#--- Settings Frame ---
+backButton = tk.Button(settingsFrame,text="back",command=lambda: gotoFrame(mainFrame))
+backButton.pack()
+
+ButtonList = [backButton,settingsButton]
 
 mainFrame.pack()
 root.mainloop()
