8a5f41476c52a416688d56fbd947e4ce7373d6f3
Dominik Engelen <dominik.engelen@online.de>
2023-10-07 21:27:47 +0200

    config basic

--- config/config.py
+++ config/config.py
@@ -1,9 +1,13 @@
 import json
 import os
 
-def SaveConfig(data,path="config\config.json"):
+def SaveConfig(newData,path="config\config.json"):
     if os.path.exists(path):
-        pass
-    else:
-        with open(path, 'w') as file:
-            json.dump(data, file, indent=4)
+        with open(path,"w") as file:
+            data = json.load(file)
+        
+        
+    data = newData
+
+    with open(path, 'w') as file:
+        json.dump(data, file, indent=4)
