17b67150238c68b227963e45a61cffbd77abb897
Dominik Engelen <dominik.engelen@online.de>
2023-10-07 22:27:18 +0200

    color modes added config big change

--- config/config.py
+++ config/config.py
@@ -1,13 +1,56 @@
 import json
 import os
 
-def SaveConfig(newData,path="config\config.json"):
-    if os.path.exists(path):
-        with open(path,"w") as file:
+def get_default_mode(json_file_path="config\config.json"):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
             data = json.load(file)
-        
-        
-    data = newData
+        return data.get("default_mode")
+    else:
+        return None  
+    
+def update_default_mode(default_mode, json_file_path="config\config.json"):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        data["default_mode"] = default_mode
+    else:
+        data = {
+            "default_mode": default_mode
+        }
+    
+    with open(json_file_path, "w") as file:
+        json.dump(data, file, indent=4)
 
-    with open(path, 'w') as file:
+def get_default_dir(json_file_path="config\config.json"):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        return data.get("default_dir", None)
+    else:
+        return None
+
+def update_default_dir(default_dir, json_file_path="config\config.json"):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        data["default_dir"] = default_dir
+    else:
+        data = {
+            "default_dir": default_dir
+        }
+    
+    with open(json_file_path, "w") as file:
+        json.dump(data, file, indent=4)
+        
+def clear_default_dir(json_file_path="config\config.json"):
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        data["default_dir"] = "null"
+    else:
+        data = {
+            "default_dir": "null"
+        }
+    with open(json_file_path, "w") as file:
         json.dump(data, file, indent=4)
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tkinter as tk
+
+from tkinter import filedialog
+from tkinter import messagebox
 from config.config import *
 from key.generator import *
 from coder.read import *
 from coder.write import *
+from PIL import Image, ImageTk
 
 global outputTextValue
 outputTextValue = ""
 
 global selectedType
 selectedType = ""
+
+Mode = "None"
+switch_value = not get_default_mode()
+
 
 
 root = tk.Tk()
@@ -17,6 +25,23 @@ root.resizable(False, False)
 root.geometry('500x500')
 root.eval("tk::PlaceWindow . center")
 
+#icon code 
+#TODO make icon
+"""
+iconDir = os.getcwd() + r"\assets\Icon.png"
+
+#set Icon
+ico = Image.open(iconDir)
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+"""
+
+
+light_open = Image.open("assets/light_mode.png")
+light = ImageTk.PhotoImage(light_open)
+
+dark_open = Image.open("assets/dark_mode.png")
+dark = ImageTk.PhotoImage(dark_open)
 
 mainFrame = tk.Frame(root)
 workFrame = tk.Frame(root)
@@ -57,10 +82,51 @@ def gotoFrame(newFrame,Type=""):
         inputKey.delete(0,tk.END)
         inputText.delete(0,tk.END)
 
-
+def clearDefKey():
+    clear_default_dir()
+    StandartKey.delete(0,tk.END)
 
 def SaveSettings():
+    update_default_mode(switch_value)
+    update_default_dir(StandartKey.get())
     gotoFrame(mainFrame)
+
+def toggle():
+  
+    global switch_value
+    if switch_value == True :
+        switch.config(image=light,bg="#26242f",
+                      activebackground="#26242f")
+
+        for frame in frame_List:
+            frame.config(bg="#26242f")
+
+        for button in ButtonList:
+            button.config(bg="#26242f", fg="#ffffff", activebackground="#26242f", activeforeground="#ffffff")
+
+        for extra in ExtraList:
+            extra.config(bg="#26242f", fg="#ffffff")
+        
+        root.config(bg="#26242f")
+        
+        switch_value = False
+  
+    else:
+        switch.config(image=dark,bg="white", 
+                      activebackground="white")
+          
+        for frame in frame_List:
+            frame.config(bg="white")
+
+        for button in ButtonList:
+            button.config(bg="white", fg="#000000", activebackground="white", activeforeground="#000000")
+
+        for extra in ExtraList:
+            extra.config(bg="white", fg="#000000")
+        
+        root.config(bg="white")
+
+        switch_value = True
 
 
 #--- Main Frame ---
@@ -82,18 +148,23 @@ writeButton.pack(pady=10)
 #--- Settings Frame ---
 
 
-backButton = tk.Button(settingsFrame,text="back",command=lambda: gotoFrame(mainFrame))
-backButton.pack(pady=10)
+setbackButton = tk.Button(settingsFrame,text="back",command=lambda: gotoFrame(mainFrame))
+setbackButton.grid(column=0,row=0,pady=10)
 
 StKeyText = tk.Label(settingsFrame,text="enter standart Key:")
-StKeyText.pack(pady=10)
+StKeyText.grid(column=0,row=1,pady=10)
 
 StandartKey = tk.Entry(settingsFrame)
-StandartKey.pack()
+StandartKey.grid(column=0,row=2)
 
+delDefKey = tk.Button(settingsFrame,text="clear",command=lambda:clearDefKey())
+delDefKey.grid(column=1,row=2)
 
-submitSettingsButton = tk.Button(settingsFrame)
+switch = tk.Button(settingsFrame, bd=0, bg="white",text="Light Mode", activebackground="white", command=toggle)
+switch.grid(column=0,row=3,pady=100)
 
+submitSettingsButton = tk.Button(settingsFrame,text="submit",command=lambda: SaveSettings())
+submitSettingsButton.grid(column=0,row=4)
 
 #--- work Frame ---
 backButton = tk.Button(workFrame,text="back",command=lambda: gotoFrame(mainFrame))
@@ -120,7 +191,9 @@ outputLabel.pack_forget()
 outputText = tk.Label(workFrame,text=outputTextValue)
 outputText.pack_forget()
 
-ButtonList = [backButton,settingsButton]
-ExtraList = [Welcome_label]
+ButtonList = [setbackButton,backButton,settingsButton,readButton,writeButton,submitButton,delDefKey,submitSettingsButton]
+ExtraList = [Welcome_label,StKeyText,StandartKey,InputTextText,inputText,KeyText,inputKey,outputLabel,]
+
 mainFrame.pack()
+toggle()
 root.mainloop()
