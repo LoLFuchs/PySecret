9ed3faf1ae6623b4e068506c5faf4e8b866b5104
Dominik Engelen <dominik.engelen@online.de>
2023-10-27 13:32:20 +0200

    Key Fix (äöü)

--- key/generator.py
+++ key/generator.py
@@ -1,13 +1,13 @@
 import string
 import random
-string.ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/([=?@€+*#-_:,;<>1234567890áéíóúÁÉÍÓÚâêîôûÂÊÎÔß '
+string.ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/([=?@€+*#-_:,;<>1234567890áéíóúÁÉÍÓÚâêîôûÂÊÎÔßöÖäÄüÜ '
 #List of sting.ascii_letters
 print(list(string.ascii_letters))
 print(len(string.ascii_letters))
 
 
 def generate(key=""):
-    while len(key) < 105:
+    while len(key) < 111:
         randomValue = random.choice(string.ascii_letters)
         if randomValue not in key:
             key += randomValue
